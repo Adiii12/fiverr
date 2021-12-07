7a4c1c2feb518b6f78c06f34d2abeaaff45c5615
Adil <marwatadil@yahoo.com>
2021-12-07 15:40:50 +0500

    fiverr web esg

---
+++ app.js
@@ -0,0 +1,16 @@
+function openMenu() {
+  var openSelector = document.getElementById("menu_list");
+  openSelector.style.marginLeft = "0px";
+}
+function closeMenu() {
+  var openSelector = document.getElementById("menu_list");
+  openSelector.style.marginLeft = "-100%";
+}
+
+// function myFunction() {
+//   var elmnt = document.getElementById("myDIV");
+//   var txt =
+//     "Height including padding and border: " + elmnt.offsetHeight + "px<br>";
+//   txt += "Width including padding and border: " + elmnt.offsetWidth + "px";
+//   alert(txt);
+// }
